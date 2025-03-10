f82e1ce0a7145e2a545317b45cf2afe85aea936d
NET15_Quốc Thịnh <thinhpdqse171589@fpt.edu.vn>
2025-03-11 06:48:30 +0700

    Refactor: update deploy

--- WebApp/speaking-ai/src/api/axiosInstance.js
+++ WebApp/speaking-ai/src/api/axiosInstance.js
@@ -2,7 +2,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://sai.runasp.net/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    (window.location.hostname.includes("vercel.app")
+      ? "/api"
+      : "http://sai.runasp.net/api"),
   headers: {
     "Content-Type": "application/json",
   },
